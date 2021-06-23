180a952704e638cd8ff93d3d2451dcb5fd85913f
luissalgueiro1985 <luisandresalgueiro@gmail.com>
2021-06-23 08:47:59 +0100

    Add project files.

---
+++ SuperShop/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperShop.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperShop.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
