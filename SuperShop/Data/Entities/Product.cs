f5fac2a8a98028d8ac248b408259edfadb021307
luissalgueiro1985 <luisandresalgueiro@gmail.com>
2021-06-23 10:25:27 +0100

    Modify products

--- SuperShop/Data/Entities/Product.cs
+++ SuperShop/Data/Entities/Product.cs
@@ -7,22 +7,29 @@ namespace SuperShop.Data.Entities
     {
         public int Id { get; set; }
 
+        [Required]
+        [MaxLength(50, ErrorMessage = "The field {0} can contain {1} characters length.")]
         public string Name { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
         public decimal Price { get; set; }
 
+
         [Display(Name = "Image")]
         public string ImageUrl { get; set; }
 
+
         [Display(Name = "Last Purchase")]
-        public DateTime LastPurchase { get; set; }
+        public DateTime? LastPurchase { get; set; }
+
 
         [Display(Name = "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
+
 
         [Display(Name = "Is Available")]
         public bool IsAvailable { get; set; }
+
 
         [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
         public double Stock { get; set; }
