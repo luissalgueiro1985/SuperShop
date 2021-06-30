1f345dcd24e6bd4a56ddeb1ce3778f94cc328c65
luissalgueiro1985 <luisandresalgueiro@gmail.com>
2021-06-30 13:16:10 +0100

    API products controler

---
+++ SuperShop/Controllers/API/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperShop.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperShop.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository) 
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(_productRepository.GetAll());
+        }
+
+
+    }
+}
